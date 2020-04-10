89e3d14df24c8613d844fea3e95a19406616e62a
aritchat <aritrachatterjee2007@gmail.com>
2020-04-10 14:54:42 +0530

    Added new code

--- DynamicProgramming/src/LongestCommonSubsequenceMemoizationDP.java
+++ DynamicProgramming/src/LongestCommonSubsequenceMemoizationDP.java
@@ -10,7 +10,12 @@
  * -> if i == m and j == n then return 0
  * The DP solution is to check, whenever we want to solve a sub problem, whether we've already done it before. So
  * we look up the solution instead of solving it again.Implemented in the most direct way, we just add some code
- * to our recursive solution. First we take care of the base cases
+ * to our recursive solution. First we take care of the base cases. We have created an LCS table with one row and one
+ * column larger than the lengths of the two strings. Then run the iterative DP loops to fill each cell in the table.
+ * This is link doing recursion backwards, or bottom up. The value of LCS[i][j] depends on three other values
+ * (LCS(i,j+1), LCS(i+1,j), LCS(i+1,j+1)) all of which have values larger than LCS[i][j]. They go through the table
+ * in the order of decreasing i and j values. This will guarantee that when we need to fill in the value of LCS[i][j],
+ * we already know the values of all cells on which it depends.
  */
 public class LongestCommonSubsequenceMemoizationDP {
     public static void main(String[] args) {
@@ -21,16 +26,31 @@ public class LongestCommonSubsequenceMemoizationDP {
     }
 
     public static String LCSLength(String X, String Y){
-        int m = X.length();
-        int n = Y.length();
-        if(m == 0 || n == 0)
-            return "";
-        else if(X.charAt(m-1) == Y.charAt(n-1))
-            return LCSLength(X.substring(0,m-1),Y.substring(0,n-1)) + X.charAt(m-1);
-        else{
-            String x = LCSLength(X,Y.substring(0,n-1));
-            String y = LCSLength(X.substring(0,m-1),Y);
-            return (x.length() > y.length()) ? x:y;
+        int[][] table = new int[X.length() + 1][Y.length() + 1];
+        //row 0 and col 0 is already initialized to 0
+        for(int i = 0; i < X.length(); i++){
+            for(int j = 0; j < Y.length(); j++){
+                if(X.charAt(i) == Y.charAt(j))
+                    table[i+1][j+1] = table[i][j] + 1;
+                else
+                    table[i+1][j+1] = Math.max(table[i+1][j], table[i][j+1]);
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
         }
+        //read the substring out from the matrix
+        StringBuffer sY = new StringBuffer();
+        for(int x = X.length(),y=Y.length(); x != 0 && y != 0;){
+            if(table[x][y] == table[x-1][y])
+                x--;
+            else if(table[x][y] == table[x][y-1])
+                y--;
+            else{
+                sY.append(X.charAt(x-1));
+                x--;
+                y--;
+            }
+        }
+        return sY.reverse().toString();
     }
 }
